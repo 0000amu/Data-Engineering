40022962758834a7979c70848d830e622cdab912
Amey Gujar <121634147+0000amu@users.noreply.github.com>
2026-09-10 12:27:55 +0530

    Create add.py to sum two command line arguments
    Add a script to sum command line arguments.

---
+++ python/Command line argument/add.py
@@ -0,0 +1,9 @@
+#to display command line args save this as cmd.py
+import sys
+
+#convert args into integers and then add:
+sum= int(sys.argv[1])+int(sys.argv[2])
+print(sum)
+
+#cd C:\Users\Amey\PycharmProjects\PythonProject
+#python Command_line_pracc.py 2 4
